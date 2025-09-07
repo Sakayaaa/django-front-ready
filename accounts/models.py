6f0315f6471db60d6486f4b1675c0fec469758b5
Amirhosein Sebghati <amir.sebghati85@gmail.com>
2025-09-07 14:15:25 +0330

    Changes to be committed: 	modified:   accounts/__pycache__/admin.cpython-313.pyc 	modified:   accounts/__pycache__/models.cpython-313.pyc 	modified:   accounts/admin.py 	new file:   accounts/migrations/0001_initial.py 	new file:   accounts/migrations/__pycache__/0001_initial.cpython-313.pyc 	modified:   accounts/models.py 	modified:   blog/__pycache__/admin.cpython-313.pyc 	modified:   blog/__pycache__/models.cpython-313.pyc 	modified:   blog/admin.py 	new file:   blog/migrations/0001_initial.py 	new file:   blog/migrations/__pycache__/0001_initial.cpython-313.pyc 	modified:   blog/models.py 	new file:   media/pfps/photo_2024-05-26_19-57-20.jpg 	modified:   weblog/__pycache__/settings.cpython-313.pyc 	modified:   weblog/__pycache__/urls.cpython-313.pyc 	modified:   weblog/settings.py 	modified:   weblog/urls.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    pfp = models.ImageField(upload_to='pfps')
+
+    def __str__(self):
+        return f"{self.user}"
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    start_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    start_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_valid = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.post.title} - {self.body[:20]}"
