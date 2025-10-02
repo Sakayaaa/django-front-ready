22bbceb23a6d43a374d3dfc6e3006d366fe5a6d5
Amirhosein Sebghati <amir.sebghati85@gmail.com>
2025-10-02 20:06:35 +0330

    Changes to be committed: 	modified:   blog/templates/blog/posts.html 	modified:   blog/urls.py 	modified:   blog/views.py 	new file:   fixture_final.json

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Comment
 from .forms import AddCommentForm, CreatePostForm
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 
 def index(request):
@@ -13,6 +14,10 @@ def index(request):
 @login_required
 def posts(request):
     posts = Post.objects.all().order_by('-updated_at')
+    
+    for post in posts:
+        post.user_liked = request.user.userprofile in post.like.all()
+        post.user_disliked = request.user.userprofile in post.dislike.all()
     
     if request.method == 'POST':
         form = CreatePostForm(request.POST)
@@ -28,7 +33,7 @@ def posts(request):
 
 @login_required
 def post(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     form = AddCommentForm()
     
     # all_comments = Comment.objects.all()
@@ -52,3 +57,36 @@ def add_comment(request, slug):
             comment.author = request.user.userprofile  # attach logged-in user
             comment.save()
     return redirect('post', slug=post.slug)  # always go back to the post page
+
+
+
+@login_required
+def like_post(request, slug):
+    user = request.user.userprofile
+    post = get_object_or_404(Post, slug=slug)
+    
+    if user in post.dislike.all():
+        post.dislike.remove(user)
+        
+    if user in post.like.all():
+        post.like.remove(user)
+    else:
+        post.like.add(user)
+        
+    return redirect('posts')
+
+
+@login_required
+def dislike_post(request, slug):
+    user = request.user.userprofile
+    post = get_object_or_404(Post, slug=slug)
+    
+    if user in post.like.all():
+        post.like.remove(user)
+        
+    if user in post.dislike.all():
+        post.dislike.remove(user)
+    else:
+        post.dislike.add(user)
+        
+    return redirect('posts')
